c9a138b59d354a3ccb4d8fbbb7f81e92282f3bde
gordonbchen <gordonbchen@gmail.com>
2025-09-17 16:48:04 -0400

    make lab6 nicer: validate user input

--- lab6.X/main.c
+++ lab6.X/main.c
@@ -9,10 +9,14 @@
 
 volatile int pin = 3;
 volatile int freq_hz = 2;
+
 volatile char asking_fp = 1;  // 1 for fp, 0 for num
-volatile char ask = 0;
-volatile char asking_forp = 'F';
+volatile char print_question = 1;
+volatile char forp = 'F';
+
 volatile char user_ans = 0;
+volatile char user_num;  // user input number for frequency / position.
+
 
 void my_delay_ms(int ms) {
     for (int i = 0; i < ms; ++i) {
@@ -21,67 +25,76 @@ void my_delay_ms(int ms) {
 }
 
 void init_clock() {
-    // Unlock protected io registers. Needed for next clock control.
     CPU_CCP = CCP_IOREG_gc;
-
-    // Enable external clock (16Mhz range).
     CLKCTRL.XOSCHFCTRLA = CLKCTRL_FRQRANGE_16M_gc | CLKCTRL_ENABLE_bm;  
-
     CPU_CCP = CCP_IOREG_gc;
-    // Use external clock source.
     CLKCTRL.MCLKCTRLA = CLKCTRL_CLKSEL_EXTCLK_gc;
 }
 
 ISR(USART3_RXC_vect) {
-    ask = 1;
+    print_question = 1;
     user_ans = USART3.RXDATAL;
 
     if (asking_fp) {
-        asking_forp = user_ans;
+        if (!((user_ans == 'F') || (user_ans == 'P'))) {
+            return;
+        }
+        forp = user_ans;
         asking_fp = 0;
         return;
     }
     
-    if (asking_forp == 'F') {
-        freq_hz = user_ans - '0';
+    user_num = user_ans - '0';
+    if ((user_num < 0) || (user_num > 9)) {
+        return;
+    }
+    if (forp == 'F') {
+        if (user_num == 0) {
+            return;
+        }
+        freq_hz = user_num;
     }
     else {
-        pin = user_ans - '0';
+        if (user_num > 7) {
+            return;
+        }
+        pin = user_num;
     }
     asking_fp = 1;
 }
 
 int main(void) {
+    // Init UART and interrupts.
     init_clock();
     uart_init(3, 9600, NULL);
     USART3.CTRLA = USART_RXCIE_bm;
     sei();
     
     VPORTD.DIR = 0b11111111;
-    
+
     int delay_ms;
-    
     while (1) {
         if (user_ans != 0) {
             putchar(user_ans);
             user_ans = 0;
         }
         
-        if (ask) {
+        if (print_question) {
             if (asking_fp) {
                 printf("\nDo you want to change the frequency or position? (F/P) ");
             }
             else {
-                if (asking_forp == 'F') {
+                if (forp == 'F') {
                     printf("\nFrequency: ");
                 }
                 else {
                     printf("\nPosition: ");
                 }
             }
-            ask = 0;
+            print_question = 0;
         }
         
+        // Blink.
         delay_ms = (1000 / freq_hz) / 2;
         VPORTD.OUT = 1 << pin;
         my_delay_ms(delay_ms);
