3906f2d357d1a442352a9f74d3744c5af7278a75
gordonbchen <gordonbchen@gmail.com>
2025-11-12 00:15:59 -0500

    don't use separate LCD i2c backend

--- rev/rev4.X/i2c.c
+++ rev/rev4.X/i2c.c
@@ -87,38 +87,26 @@ uint8_t TWI_read(TWI_t* twi, uint8_t addr, uint8_t cmd) {
 
 
 // LCD.
-void LCD_Backlight_write(uint8_t addr, uint8_t v1, uint8_t v2) {
-    TWI1.MADDR = addr << 1;
-    while (!(TWI1.MSTATUS & TWI_WIF_bm));
+// void LCD_Backlight_write(uint8_t addr, uint8_t v1, uint8_t v2) {
+//     TWI1.MADDR = addr << 1;
+//     while (!(TWI1.MSTATUS & TWI_WIF_bm));
 
-    TWI1.MDATA = v1;
-    while (!(TWI1.MSTATUS & TWI_WIF_bm));
+//     TWI1.MDATA = v1;
+//     while (!(TWI1.MSTATUS & TWI_WIF_bm));
 
-    TWI1.MDATA = v2;
-    while (!(TWI1.MSTATUS & TWI_WIF_bm));
+//     TWI1.MDATA = v2;
+//     while (!(TWI1.MSTATUS & TWI_WIF_bm));
 
-    TWI1.MCTRLB = TWI_MCMD_STOP_gc;
-}
+//     TWI1.MCTRLB = TWI_MCMD_STOP_gc;
+// }
 
 void LCD_write(uint8_t mode, uint8_t data) {
-    LCD_Backlight_write(LCD_ADDR, mode, data);
-}
-
-void Backlight_write(uint8_t reg, uint8_t data) {
-    LCD_Backlight_write(BACKLIGHT_ADDR, reg, data);
-}
-
-void Backlight_init() {
-    _delay_ms(50);
-    Backlight_write(0x2F, 0x00);  // Reset to default.
-    Backlight_write(0x04, 0xFF);  // Set max brightness.
-    Backlight_write(0x00, 0x20);  // Set software shutdown.
-    Backlight_write(0x07, 0x00);  // Update registers.
-    _delay_ms(50);
+    TWI_write(&TWI1, LCD_ADDR, mode, data);
+//     LCD_Backlight_write(LCD_ADDR, mode, data);
 }
 
 void LCD_init() {
-    _delay_ms(50);                  // Wait for LCD reset
+    _delay_ms(50);                          // Wait for LCD reset
     LCD_write(LCD_CMD_MODE, 0x38);  // Function set: 2 lines, 5x8 font
     LCD_write(LCD_CMD_MODE, 0x0C);  // Display ON, cursor OFF
     LCD_write(LCD_CMD_MODE, 0x01);  // Clear display
@@ -141,4 +129,19 @@ void LCD_print(const char *str) {
 void LCD_clear() {
     LCD_write(LCD_CMD_MODE, 0x01);  // clear screen.
     LCD_write(LCD_CMD_MODE, 0x02);  // return home.
+}
+
+
+void Backlight_write(uint8_t reg, uint8_t data) {
+    TWI_write(&TWI1, BACKLIGHT_ADDR, reg, data);
+//     LCD_Backlight_write(BACKLIGHT_ADDR, reg, data);
+}
+
+void Backlight_init() {
+    _delay_ms(50);
+    Backlight_write(0x2F, 0x00);  // Reset to default.
+    Backlight_write(0x04, 0xFF);  // Set max brightness.
+    Backlight_write(0x00, 0x20);  // Set software shutdown.
+    Backlight_write(0x07, 0x00);  // Update registers.
+    _delay_ms(50);
 }
--- rev/rev4.X/i2c.h
+++ rev/rev4.X/i2c.h
@@ -5,6 +5,7 @@
 #include <util/delay.h>
 #include <util/twi.h>
 
+
 #define TWI_WRITE_MODE 0
 #define TWI_READ_MODE 1
 
@@ -23,6 +24,7 @@ uint8_t TWI_read(TWI_t* twi, uint8_t addr, uint8_t cmd);
 
 #define TC74_ADDR 0x48
 #define TC74_READ_TEMP_CMD 0x00
+
 static inline int read_temp_i2c() {
     return TWI_read(&TWI0, TC74_ADDR, TC74_READ_TEMP_CMD);
 }
@@ -31,19 +33,16 @@ static inline int read_temp_i2c() {
 #define LCD_ADDR 0x3E
 #define LCD_CMD_MODE 0x00
 #define LCD_DATA_MODE 0x40
-// static inline void LCD_write(uint8_t mode, uint8_t data) {
-//     TWI_write(&TWI1, LCD_ADDR, mode, data);
-// }
 
-#define BACKLIGHT_ADDR 0x6B
-// static inline void Backlight_write(uint8_t reg, uint8_t data) {
-//     TWI_write(&TWI1, BACKLIGHT_ADDR, reg, data);
-// }
-
-
-void Backlight_init();
+void LCD_write(uint8_t mode, uint8_t data);
 void LCD_init();
 void LCD_print(const char *str);
 void LCD_clear();
+
+
+#define BACKLIGHT_ADDR 0x6B
+
+void Backlight_write(uint8_t reg, uint8_t data);
+void Backlight_init();
 
 #endif
